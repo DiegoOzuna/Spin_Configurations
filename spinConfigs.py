4058b852a609e611a0ea69d46c07a4cd3cff0875
DiegoOzuna <ozunad2002@gmail.com>
2024-06-18 14:43:59 -0500

    Update: made lattice be a class struct; redefined

--- spinConfigs.py
+++ spinConfigs.py
@@ -1,84 +1,43 @@
 #This file's main purpose is to just configure an Initial Spin Configuration and Spin Flips.
+#It is also meant to handle operations done on the lattice itself.
 
 import numpy as np
 
-n = 0   #lattice dimensions nxn
+class Lattice:
+    def __init__(self, n):      #This function will create our initial lattice space.
+        self.n = n
+        self.config = np.zeros((n,n))
+        self.fill_lattice()
 
-#This function will create our initial lattice space.
-def initializeLattice(n):
-    setN(n)                      
-    lattice = np.zeros((n,n))
-    fillLattice(lattice, n)
-    return(lattice)
+    def fill_lattice(self):     #This function is meant to fill in our initial lattice space with "spins" (up/down)
+        for x in range(self.n):
+            for y in range(self.n):
+                self.config[x][y] = np.random.choice([1, -1])
 
-#This function is meant to fill in our initial lattice space with "spins" (up/down)
-def fillLattice(lattice, n):
-    for x in range(n):
-        for y in range(n):
-            lattice[x][y] = np.random.choice([1, -1])
+    def neighboring_cost(self, i, j):       #This function is meant to calculate the local cost of S_ij 
+        currentNode = self.config[i][j]
+        aboveNode = self.config[i][(j+1)%self.n]
+        belowNode = self.config[i][(j-1)%self.n]
+        rightNode = self.config[(i+1)%self.n][j]
+        leftNode = self.config[(i-1)%self.n][j]
 
-#this will be used to store the n size of the lattice
-def setN(N):
-    global n
-    n = N   
+        return np.multiply(currentNode, [aboveNode+belowNode+rightNode+leftNode])
 
-#this will be used in other functions where n cant/shouldnt be passed through user input
-def getN():
-    return n
-
-
-#This function is meant to calculate the local cost of S_ij 
-def neighboringCost(lattice, i, j):
-    n=getN()
-    currentNode = lattice[i][j]
-    aboveNode = lattice[i][(j+1)%n]
-    belowNode = lattice[i][(j-1)%n]
-    rightNode = lattice[(i+1)%n][j]
-    leftNode = lattice[(i-1)%n][j]
-
-    return np.multiply(currentNode, [aboveNode+belowNode+rightNode+leftNode])
-
-#This function is meant to just flip the spin of S_ij passed...
-def spinFlip(lattice, i, j):
-    lattice[i][j] *= -1
+    def spin_flip(self, i, j):          #This function is meant to just flip the spin of S_ij passed...
+        self.config[i][j] *= -1
 
 
 ########################################################################################
-# below was inital lattice example, to then generalize above...
-# lattice = np.zeros((32,32))
+# # below is testing out the reconfiguration (aka our class lattice)
 
-# print("Initial Empty Lattice Space")
-# print(lattice)
+# lattice = Lattice(3)       #should make the space of the lattice, and fill it in with spins (+1 / -1)
 
-# for x in range(32):
-#         for y in range(32):
-#             lattice[x][y] = np.random.choice([1, -1])
+# print(lattice.config)       #this should show the lattice space
 
-# print("Initial Lattice Space")
-# print(lattice)
+# print(lattice.neighboring_cost(0,0))    #this would give us the cost, should be through periodic bounds
+# print(lattice.neighboring_cost(2,2))    #this would give us the cost, should be through periodic bounds
 
-########################################################################################
-# below was testing of functions. Along with testing of new function neighboringCost
-# lattice = initializeLattice(8)
+# lattice.spin_flip(1,1)                  #flip middle spin
 
-# print(lattice)
+# print(lattice.config)                   #should see middle spin be opposite
 
-# cost = neighboringCost(lattice, 4, 4) #test to see if calculation of cost works
-# cost2 = neighboringCost(lattice, 7, 0) #test to see if we have periodic bounds
-
-# print(cost)
-# print()
-# print(cost2)
-
-########################################################################################
-# below is testing just the spin flip...
-
-# lattice = initializeLattice(2)
-
-# print(lattice)
-
-# spinFlip(lattice, 0,0)
-
-# print()
-
-# print(lattice)
