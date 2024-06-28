d8bf5eed34b37c31ceb04bc69acc8d4a402e1638
DiegoOzuna <ozunad2002@gmail.com>
2024-06-28 01:21:20 -0500

    Update: Made three changes. Read More. Changed storeData to format data correctly. fix, edited monteCarlo to create multi-initial configurations. fix, edited calculateTotalEnergy to properly calculate energy.

--- configMetropolis.py
+++ configMetropolis.py
@@ -8,9 +8,15 @@ import multiprocessing as mp
 import spinConfigs # will be holding the lattice functions...
 
 
-def storeData(magnetTempData, fileName):
-    df = pd.DataFrame(magnetTempData, columns=['Temperature', 'Magnetization'])
-    df.to_csv(fileName, index=False)
+def storeData(dictionary, filename):
+    # Convert the dictionary to a DataFrame
+    df = pd.DataFrame(list(dictionary.items()), columns=['Temperature', 'Magnetization'])
+
+    # Convert the lists in the 'Magnetization' column to string
+    df['Magnetization'] = df['Magnetization'].apply(lambda x: str(x))
+
+    # Save the DataFrame to a CSV file
+    df.to_csv(filename, index=False)
 
 ########################################################################################################################
 # purpose: montecarlo will simply generate a list of magnetizations after applying a metropolis algorithm where we  
@@ -22,9 +28,9 @@ def storeData(magnetTempData, fileName):
 # Temp; the max temperature of our system (will start from 1 and go to Temp in 0.5 increments)
 # step; the MCS step we want to consistently keep measurements on (ex: every 1000th step we measure our system)
 ########################################################################################################################
-def montecarlo(lattice, MCS, maxTemp, step):
+def montecarlo(MCS, maxTemp, step):
     temp_steps = np.arange(1,maxTemp,0.5)
-    latticeConfigurations = [0] * temp_steps.size                 # each index corresponds to a temperature (spin1 corresponds to temp 1)
+    latticeConfigurations = [spinConfigs.Lattice(4) for _ in range(temp_steps.size)]    # each index corresponds to a temperature (spin1 corresponds to temp 1)
     Elist = [0] * temp_steps.size                                 # each index corresponds to a temperature (spin1 corresponds to temp 1)
     
     Mlist = {t: [0] * (MCS//step) for t in temp_steps}  # we know at each MCS we save configurations at different temperatures (ie we know we need # of unique
@@ -33,12 +39,12 @@ def montecarlo(lattice, MCS, maxTemp, step):
 
     index = 0       #This index is used in order to store our data correctly within the Mlist every 1000 steps
     
-    n = lattice.n
+    n = latticeConfigurations[0].n
 
     for z in tqdm(range(MCS)):
         # do singular spin flips first
         for t in range(temp_steps.size):
-            latticeConfigurations[t] = single_spin_flips(lattice, temp_steps[t])      # store final lattice from single_spin_flip for specific temperature
+            latticeConfigurations[t] = single_spin_flips(latticeConfigurations[t], temp_steps[t])      # store final lattice from single_spin_flip for specific temperature
 
         # Calculate total energy of systems
         for t in range(temp_steps.size):
@@ -79,11 +85,11 @@ def montecarlo(lattice, MCS, maxTemp, step):
 # lattice; the configuration of the 2d lattice (nxn)
 ########################################################################################################################   
 def calculateTotalEnergy(lattice):
-    L = int(np.sqrt(lattice.n))     # L^2 = N 
+    L = lattice.n     # L^2 = N ; therefore we only sum two sides
     E = 0
-    for x in range(0, L):
-        for y in range(0, L):
-            E = E + lattice.config[x][y] * (lattice.config[(x+1)%L][y] + lattice.config[x][(y+1)%L])
+    for x in range(L):
+        for y in range(L):
+            E = E - lattice.config[x][y] * (lattice.config[(x+1)%L][y] + lattice.config[x][(y+1)%L])
     
     return E
 
@@ -118,39 +124,45 @@ def single_spin_flips(lattice, temp):            #will apply single spin flips t
     new_lattice.config = np.copy(lattice.config)
     return new_lattice
 
-########################################################################################################################
-# Purpose: The purpose of equalibriated is to make sure that we have the system's lattice be closer to the probability
-#          distribution (in theory exp(-(E(alpha)/kT)))
-#
-# Params:
-# lattice; the initial configuration of the 2d lattice (nxn)
-# temp; the temperature of the system
-########################################################################################################################            
-def equalibriated(lattice, temp):      #will assume that we could do 10000 steps to achieve an equalibriated state...
-    for z in range(10000):
-        for i in range(lattice.n):
-            for j in range(lattice.n):
-                energy_of_flip = (2) * lattice.neighboring_cost(i,j)
-                
-                if(energy_of_flip <= 0):
-                    lattice.spin_flip(i,j)
-                else:
-                    random = np.random.random() #generates some float between 0 and 1
-                    if random < np.exp(-energy_of_flip / temp):
-                        lattice.spin_flip(i, j)
-    
-    new_lattice = spinConfigs.Lattice(lattice.n)
-    new_lattice.config = np.copy(lattice.config)
-    return new_lattice
 
 
 
 
-lattice = spinConfigs.Lattice(4) #4x4 lattice
 maxTemp = 5
 MonteCarloSteps = 10**5
 measureEvery = 1000
 
-array = montecarlo(lattice, MonteCarloSteps, maxTemp, measureEvery)
+tempMagPairs = montecarlo(MonteCarloSteps, maxTemp, measureEvery)
 
-print(array)
+storeData(tempMagPairs, "Magnetization_4x4.csv")
+
+
+
+
+
+
+
+########################################################################################################################
+# # Purpose: The purpose of equalibriated is to make sure that we have the system's lattice be closer to the probability
+# #          distribution (in theory exp(-(E(alpha)/kT)))
+# #
+# # Params:
+# # lattice; the initial configuration of the 2d lattice (nxn)
+# # temp; the temperature of the system
+# ########################################################################################################################            
+# def equalibriated(lattice, temp):      #will assume that we could do 10000 steps to achieve an equalibriated state...
+#     for z in range(10000):
+#         for i in range(lattice.n):
+#             for j in range(lattice.n):
+#                 energy_of_flip = (2) * lattice.neighboring_cost(i,j)
+                
+#                 if(energy_of_flip <= 0):
+#                     lattice.spin_flip(i,j)
+#                 else:
+#                     random = np.random.random() #generates some float between 0 and 1
+#                     if random < np.exp(-energy_of_flip / temp):
+#                         lattice.spin_flip(i, j)
+    
+#     new_lattice = spinConfigs.Lattice(lattice.n)
+#     new_lattice.config = np.copy(lattice.config)
+#     return new_lattice
